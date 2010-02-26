f97bfc69b8c44b370c1430bb0c8f6ef13abfa9a3
Kirby Turner <kirby@thecave.com>
2010-02-26 14:07:50 -0500

    Improved scale and crop algorithm.

--- src/Sample/Classes/UIImage+KTCategory.m
+++ src/Sample/Classes/UIImage+KTCategory.m
@@ -52,6 +52,7 @@
    // the inner most parts of the image.
    CGFloat offsetX = 0;
    CGFloat offsetY = 0;
+   imageSize = [scaledImage size];
    if (imageSize.width < imageSize.height) {
       offsetY = (imageSize.height / 2) - (imageSize.width / 2);
    } else {
@@ -69,62 +70,5 @@
    return newImage;
 }
 
-- (UIImage *)old_imageScaleAndCropToMaxSize:(CGSize)newSize {
-
-   CGFloat offsetX = 0;
-   CGFloat offsetY = 0;
-
-   // Crop the image based on the new size aspect ratio.
-   CGSize imageSize = [self size];
-   if (imageSize.width < imageSize.height) {
-      offsetY = (imageSize.height / 2) - (imageSize.width / 2);
-   } else {
-      offsetX = (imageSize.width / 2) - (imageSize.height / 2);
-   }
-
-   CGRect cropRect = CGRectMake(offsetX, offsetY,
-                                imageSize.width - (offsetX * 2),
-                                imageSize.height - (offsetY * 2));
-
-   CGImageRef croppedImageRef = CGImageCreateWithImageInRect([self CGImage], cropRect);
-
-   // Draw a scaled version of the image.
-   CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-   CGContextRef context = CGBitmapContextCreate(NULL, 
-                                                newSize.width,
-                                                newSize.height,
-                                                8, 0, colorSpace,
-                                                kCGImageAlphaPremultipliedFirst);
-   CGColorSpaceRelease(colorSpace);
-
-   switch ([self imageOrientation]) {
-      case UIImageOrientationDown:
-         CGContextTranslateCTM(context, newSize.width, newSize.height);
-         CGContextRotateCTM(context, 180 * (M_PI/180));
-         break;
-      case UIImageOrientationLeft:
-         CGContextTranslateCTM(context, newSize.height, 0);
-         CGContextRotateCTM(context, 90 * (M_PI/180));
-         break;
-      case UIImageOrientationRight:
-         CGContextTranslateCTM(context, 0, newSize.width);
-         CGContextRotateCTM(context, -90 * (M_PI/180));
-         break;
-      default:
-         break;
-   }
-   
-   CGContextDrawImage(context, CGRectMake(0, 0, newSize.width, newSize.height), croppedImageRef);
-   CGContextFlush(context);
-   CGImageRef scaledImageRef = CGBitmapContextCreateImage(context);
-   CGContextRelease(context);
-
-   UIImage *newImage = [UIImage imageWithCGImage:scaledImageRef];
-   
-   CGImageRelease(croppedImageRef);
-   CGImageRelease(scaledImageRef);
-   
-   return newImage;
-}
 
 @end
