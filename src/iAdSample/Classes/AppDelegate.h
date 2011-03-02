f7943e910c22b235bb725de5172e3fbf1a638594
Kirby Turner <kirby@thecave.com>
2011-03-02 06:32:43 -0500

    Initial import of sample project.

---
+++ src/iAdSample/Classes/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  iAdSampleAppDelegate.h
+//  iAdSample
+//
+//  Created by Kirby Turner on 3/2/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> 
+{
+   UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ src/iAdSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iAdSample
+//
+//  Created by Kirby Turner on 3/2/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
