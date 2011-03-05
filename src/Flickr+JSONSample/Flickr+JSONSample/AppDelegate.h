9110dd2c6bf5c656069006ef7d9238b2ed802896
Kirby Turner <kirby@thecave.com>
2011-03-05 15:13:39 -0500

    Initial import of Flickr+JSON sample app.

---
+++ src/Flickr+JSONSample/Flickr+JSONSample/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Flickr_JSONSampleAppDelegate.h
+//  Flickr+JSONSample
+//
+//  Created by Kirby Turner on 3/5/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> 
+{
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ src/Flickr+JSONSample/Flickr+JSONSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Flickr+JSONSample
+//
+//  Created by Kirby Turner on 3/5/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+   int retVal = UIApplicationMain(argc, argv, nil, nil);
+   [pool release];
+   return retVal;
+}
