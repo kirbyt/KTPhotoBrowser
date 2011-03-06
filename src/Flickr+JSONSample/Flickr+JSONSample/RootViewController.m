7cc0c7b023559d14b93ec9bd464f5c0c5a765548
Kirby Turner <kirby@thecave.com>
2011-03-06 12:19:31 -0500

    Added default images. Added activity indicator.

--- src/Flickr+JSONSample/Flickr+JSONSample/RootViewController.m
+++ src/Flickr+JSONSample/Flickr+JSONSample/RootViewController.m
@@ -10,14 +10,18 @@
 #import "FlickrPhotosDataSource.h"
 
 @interface RootViewController ()
+@property (nonatomic, retain) UIActivityIndicatorView *activityIndicator;
 - (FlickrPhotosDataSource *)photos;
 @end
 
 @implementation RootViewController
 
+@synthesize activityIndicator = activityIndicator_;
+
 - (void)dealloc
 {
    [photos_ release], photos_ = nil;
+   [activityIndicator_ release], activityIndicator_ = nil;
    [super dealloc];
 }
 
@@ -25,13 +29,26 @@
 {
    [super viewDidLoad];
    
-   [self setDataSource:[self photos]];
-   [self setTitle:[NSString stringWithFormat:@"%i Photos", [[self photos] numberOfPhotos]]];
-   
+   UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+   [activityIndicator setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin];
+   [activityIndicator setCenter:[[self view] center]];
+   [activityIndicator startAnimating];
+   [self setActivityIndicator:activityIndicator];
+   [activityIndicator release];
+
+   [[self view] addSubview:[self activityIndicator]];
+
    // Label back button as "Back".
    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Back", @"Back button title") style:UIBarButtonItemStylePlain target:nil action:nil];
    [[self navigationItem] setBackBarButtonItem:backButton];
    [backButton release];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+   [self setDataSource:[self photos]];
+   [self setTitle:[NSString stringWithFormat:@"%i Photos", [[self photos] numberOfPhotos]]];
+   [[self activityIndicator] stopAnimating];
 }
 
 - (FlickrPhotosDataSource *)photos
